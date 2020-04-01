dc15b3df2440a6333740e771e72dc1ffafb37a7e
MihiroSaito <61680540+MihiroSaito@users.noreply.github.com>
2020-04-01 15:08:03 +0900

    haml

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,15 +4,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  GET /resource/registrations
+  def index
+    super
+  end
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+  GET /resource/registrations/sign_up
+  def new
+    super
+  end
+
+  POST /resource/registrations
+  def create
+    super
+  end
 
   # GET /resource/edit
   # def edit
