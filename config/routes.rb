8e4b6d14b657f0e4bb23c9b33e2e134e584cf31b
MihiroSaito <61680540+MihiroSaito@users.noreply.github.com>
2020-04-01 16:55:50 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_for :users
+  devise_scope :user do
+    get 'users/home'=>'devise/registrations#index'
+  end
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
