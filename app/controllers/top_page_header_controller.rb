c4a59e92738b529346517811acedd46b8b80e0f0
nakakouchi <61769004+nakakouchi@users.noreply.github.com>
2020-04-01 20:39:17 +0900

    top-page_header作成

--- app/controllers/top_page_header_controller.rb
+++
@@ -1,4 +0,0 @@
-class TopPageHeaderController < ApplicationController
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'top_page_header/index'
-  root "top_page_header#index"
+  root "top_page#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
