071bcbc51e594ed7c26d25011361ea0bb9882ae8
reinatakemura <sleepfreemusic.tb@gmail.com>
2020-04-02 17:59:31 +0900

    additional users_info column into users_table

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,12 @@
 ActiveRecord::Schema.define(version: 2020_04_01_035402) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "last_name_kana", null: false
+    t.string "phone_number"
+    t.date "birthday", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
