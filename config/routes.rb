f595b5cf075066447129c26f985d7b888045e764
syou2000 <syoutwilight@gmail.com>
2020-04-07 12:15:45 +0900

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root 'exhibition#index'
+  root 'top_page#index'
+  resources :users, only: [:new]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "items/sample_show", to: "items#sample_show"
+  get "items/sample_show2", to: "items#sample_show2"
 end
