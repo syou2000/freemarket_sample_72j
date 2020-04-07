e522de9078762681d730039354259c61ade89eb6
ken111626 <setsuko.t-09.26-gonzales@docomo.ne.jp>
2020-04-07 15:58:43 +0900

    items_table_create
    items、items-imagesのテーブルを作成しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  root 'exhibition#index'
+  root 'top_page#index'
+  resources :users, only: [:new]
+  resources :items, only: [:create,:destroy]
+  get 'exhibition/index'
+  get "items/sample_show", to: "items#sample_show"
+  get "items/sample_show2", to: "items#sample_show2"
 end
---
+++ db/schema.rb
@@ -0,0 +1,43 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_04_07_062250) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "price", null: false
+    t.text "explain", null: false
+    t.integer "postage", null: false
+    t.string "region", null: false
+    t.string "state", null: false
+    t.integer "shipping_date", null: false
+    t.integer "size"
+    t.integer "brand_id", null: false
+    t.integer "category_id", null: false
+    t.integer "user_id", null: false
+    t.integer "item_status", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_items_on_name"
+    t.index ["price"], name: "index_items_on_price"
+  end
+
+  create_table "items_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_items_images_on_item_id"
+  end
+
+  add_foreign_key "items_images", "items"
+end
