607bedc752f7e064c08133fbf479d6f4ff1ae908
MihiroSaito <61680540+MihiroSaito@users.noreply.github.com>
2020-04-10 15:15:03 +0900

    fixed view
    商品出品ページの必須ボタンの大きさを変えた

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,5 +1,4 @@
 class ItemImage < ApplicationRecord
   mount_uploader :image, ItemImageUploader
-  
   belongs_to :item
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_09_065247) do
+ActiveRecord::Schema.define(version: 2020_04_10_043123) do
 
   create_table "item_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image", null: false
