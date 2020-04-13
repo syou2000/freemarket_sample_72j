e99112a9d407e8586477d80490c9115ed8b86739
syou2000 <syoutwilight@gmail.com>
2020-04-13 11:58:56 +0900

    correct

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -64,3 +64,10 @@ server '18.178.6.249', user: 'ec2-user', roles: %w{app db web}
 
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
+
+set :ssh_options, {
+  port: 3000,#XXXにポート番号
+  keys: [File.expand_path('~/.ssh/id_rsa')],#'~/.ssh/id_rsa'部分をローカル環境の鍵のパスに
+  forward_agent: true,
+  auth_methods: %w(publickey)
+}
