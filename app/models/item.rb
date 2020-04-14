edbc1d71b33a9acf0738d1d7e8cae2c4bc7850b7
MihiroSaito <61680540+MihiroSaito@users.noreply.github.com>
2020-04-14 21:23:22 +0900

    correct conflict
    コンフリクトの解消

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,23 @@
 class Item < ApplicationRecord
   # has_many :comments
   # has_many :messages
-  has_many :item_images
+  has_many :item_images, dependent: :destroy
   accepts_nested_attributes_for :item_images, allow_destroy: true
-  # belongs_to :user
-  # belongs_to :brand
   # belongs_to :categry
   # has_one :late
+  belongs_to :user
+  belongs_to :brand
+  has_one :buyer
+  def item_image
+    return ItemsImage.find_by(item_id: self.id)
+  end
+    # itemモデルとuserモデルのアソシエーションはうまくいっている、参考までに残しているが、最後には削除する
+  # def user
+  #   return User.find_by(id: self.user_id)
+  # end
 
   validates :name,            presence: true
   validates :price,           presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-  # validates :price,           presence: true, format: { with: [300-9999999] }
   validates :explain,         presence: true
   validates :postage,         presence: true
   validates :prefecture,      presence: true
@@ -18,3 +25,4 @@ class Item < ApplicationRecord
   validates :category,        presence: true
   validates :item_status,     presence: true
 end
+
--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -2,3 +2,4 @@ class ItemImage < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :item, optional: true
 end
+
