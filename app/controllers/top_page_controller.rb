b09482d37cd46a489005aa7a10e713e4895eec9b
reinatakemura <sleepfreemusic.tb@gmail.com>
2020-04-14 14:03:00 +0900

    code_review_of_shingoteshima third
    先ほどご指摘いただいたfindの部分ですが、コメントへの返信に記載の通り、
    エラー解消のためにこの表記になっております。

--- app/controllers/top_page_controller.rb
+++ app/controllers/top_page_controller.rb
@@ -1,9 +1,8 @@
 class TopPageController < ApplicationController
   def index
     @items = Item.order(created_at: :desc)
-    @item = Item.find(params[:id])
-    # @item = Item.find_by(id: params[:id])
-    @brands = Brand.all.order(created_at: :desc)
+    @item = Item.find_by(id: params[:id])
+    @brands = Brand.order(created_at: :desc)
   end
 
 end
