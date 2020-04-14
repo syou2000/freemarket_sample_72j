3f99a901a05c7137080af53ca6ba3b4d8895b185
syou2000 <syoutwilight@gmail.com>
2020-04-14 17:38:10 +0900

    correct

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   has_many :item_images, dependent: :destroy
-  belongs_to :category
+  # belongs_to :category
   belongs_to :user
   belongs_to :brand
   has_one :buyer
