1eefcd659621717d3c2b4b2ada136df5c7e2cb9a
MihiroSaito <61680540+MihiroSaito@users.noreply.github.com>
2020-04-14 21:30:16 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,6 @@ class Item < ApplicationRecord
   # def user
   #   return User.find_by(id: self.user_id)
   # end
-
   validates :name,            presence: true
   validates :price,           presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :explain,         presence: true
@@ -25,4 +24,3 @@ class Item < ApplicationRecord
   validates :category,        presence: true
   validates :item_status,     presence: true
 end
-
