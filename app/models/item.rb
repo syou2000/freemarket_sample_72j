9477af394eeb2edbf9dbf67c50085f30e55e555a
nakakouchi <61769004+nakakouchi@users.noreply.github.com>
2020-04-16 11:22:34 +0900

    release "comment out"

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,13 +22,13 @@ class Item < ApplicationRecord
   #   return User.find_by(id: self.user_id)
   # end
 
-  # validates :name,            presence: true
-  # validates :price,           presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-  # validates :explain,         presence: true
-  # validates :postage,         presence: true
-  # validates :prefecture,      presence: true
-  # validates :shipping_date,   presence: true
-  # validates :category,        presence: true
-  # validates :item_status,     presence: true
+  validates :name,            presence: true
+  validates :price,           presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :explain,         presence: true
+  validates :postage,         presence: true
+  validates :prefecture,      presence: true
+  validates :shipping_date,   presence: true
+  validates :category,        presence: true
+  validates :item_status,     presence: true
 
 end
