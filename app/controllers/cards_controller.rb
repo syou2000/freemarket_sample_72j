ee9ccde135c70e5e7fa1384a75dbd7785c9ea5f7
MihiroSaito <61680540+MihiroSaito@users.noreply.github.com>
2020-04-16 22:34:31 +0900

    Update cards_controller.rb

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -11,22 +11,24 @@ class CardsController < ApplicationController
     @side2 = ["プロフィール","発送元・お届け先住所変更","メール/パスワード","本人情報","電話番号の確認"]
   end
 
-  def create #payjpとCardのデータベース作成を実施します。
+  def create #PayjpとCardのデータベースを作成
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+
     if params['payjp-token'].blank?
       redirect_to action: "new"
     else
+      # トークンが正常に発行されていたら、顧客情報をPAY.JPに登録します。
       customer = Payjp::Customer.create(
-      description: '登録テスト', #なくてもOK
-      email: current_user.email, #なくてもOK
-      card: params['payjp-token'],
-      metadata: {user_id: current_user.id}
-      ) #念の為metadataにuser_idを入れましたがなくてもOK
+        description: 'test', # 無くてもOK。PAY.JPの顧客情報に表示する概要です。
+        email: current_user.email,
+        card: params['payjp-token'], # 直前のnewアクションで発行され、送られてくるトークンをここで顧客に紐付けて永久保存します。
+        metadata: {user_id: current_user.id} # 無くてもOK。
+      )
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
-        redirect_to action: "show"
+        redirect_to "index"
       else
-        redirect_to action: "pay"
+        redirect_to action: "create"
       end
     end
   end
