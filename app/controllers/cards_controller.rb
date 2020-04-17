30f3fa15ae446711d7a7fdb18c69d4c508654764
MihiroSaito <61680540+MihiroSaito@users.noreply.github.com>
2020-04-17 15:49:47 +0900

    add view
    indexのビューを追加した

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -36,6 +36,9 @@ class CardsController < ApplicationController
   end
 
   def index #CardのデータをPayjpに送って情報を取り出す
+    @side = ["マイページ","お知らせ","やることリスト","いいね！一覧","出品する","下書き一覧","出品した商品-出品中","出品した商品-取引中","出品した商品-売却済み","購入した商品-取引中","購入した商品-過去の取引","ニュース一覧","評価一覧","ガイド","お問い合わせ"]
+    @side1 = ["売上・振込申請","ポイント"]
+    @side2 = ["プロフィール","発送元・お届け先住所変更","メール/パスワード","本人情報","電話番号の確認"]
     if @card.present?
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(@card.customer_id)
