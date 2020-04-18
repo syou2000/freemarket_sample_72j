4929bca1a9034bf0557eddb884790bbdd82a7f63
MihiroSaito <61680540+MihiroSaito@users.noreply.github.com>
2020-04-18 17:21:32 +0900

    correct view
    ビューのエラーを解決した

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,62 +1,71 @@
 class ItemsController < ApplicationController
   before_action :set_item, only:[:show, :destroy, :purchase, :payment]
-
+  
   def new
-    @item = Item.new
-    @item.item_images.new
+  @item = Item.new
+  @item.item_images.new
   end
-
+  
   def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path 
-    else
-      flash.now[:alert] = '入力に誤りがあります'
-      render :new
-    end
+  @item = Item.new(item_params)
+  if @item.save
+  redirect_to root_path
+  else
+  flash.now[:alert] = '入力に誤りがあります'
+  render :new
   end
-
+  end
+  
   def show
   end
-
+  
   def destroy
-    @item.destroy
-    redirect_to root_path
+  @item.destroy
+  redirect_to root_path
   end
-
-  def edit 
+  
+  def edit
   end
-
+  
   def purchase
-    @address = Address.find(params[:id])
-    @card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
-    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-    customer = Payjp::Customer.retrieve(@card.customer_id)
-    @card_information = customer.cards.retrieve(@card.card_id)
+  @address = Address.find(params[:id])
+  @card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
+  if @card.present?
+  Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+  customer = Payjp::Customer.retrieve(@card.customer_id)
+  @card_information = customer.cards.retrieve(@card.card_id)
+  Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+  Payjp::Charge.create(
+  amount: @item.price,
+  :customer => @card.customer_id,
+  currency: 'jpy'
+  )
   end
-
+  end
+  
   def payment
-    @buyer = Buyer.new(user_id: current_user.id, item_id: params[:id])
-    if @buyer.save
-      redirect_to complete_path
-    else
-      render :purchase
-    end
+  @buyer = Buyer.new(user_id: current_user.id, item_id: params[:id])
+  if @buyer.save
+  redirect_to complete_path
+  else
+  render :purchase
   end
-
+  end
+  
   def complete
   end
-
+  
   private
   def item_params
-    params.require(:item).permit(:name, :price, :explain, :postage, :brand, :category, :prefecture_id, :shipping_date, :item_status, item_images_attributes: [:image, :_destroy, :id]).merge(user_id: current_user.id)
+  params.require(:item).permit(:name, :price, :explain, :postage, :brand, :category, :prefecture_id, :shipping_date, :item_status, item_images_attributes: [:image, :_destroy, :id]).merge(user_id: current_user.id)
   end
-
+  
   def user_params
-    params.require(:user).premit(:buyer_id, :exhibitor_id)
+  params.require(:user).premit(:buyer_id, :exhibitor_id)
   end
-
+  
   def set_item
-    @item = Item.find(params[:id])
+  @item = Item.find(params[:id])
   end
-end
+  end
+  
