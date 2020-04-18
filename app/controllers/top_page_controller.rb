f9cd2c6c7c01272aa84bcf7a89d13b4df3819d3f
MihiroSaito <61680540+MihiroSaito@users.noreply.github.com>
2020-04-18 21:26:44 +0900

    Update top_page_controller.rb

--- app/controllers/top_page_controller.rb
+++ app/controllers/top_page_controller.rb
@@ -3,6 +3,14 @@ class TopPageController < ApplicationController
     @items = Item.includes(:item_images).order(created_at: :desc)
     @item = Item.find_by(id: params[:id])
     @brands = @items.where(brand: "シャネル").order(created_at: :desc)
+    @images = ItemImage.order(created_at: :desc)
+    # @buyer = Buyer.find_by(id: params[:id])
+    @buyerBrand = Buyer.pluck(:item_id)
+    # @buyerBrand = @buyer.item_id
   end
+  
+def product_buyer
+  params.require(:item).permit(:id, :buyer_id)
+end
 
 end
